90def47268d862c27096f8f41ff64f711b580d50
AkhilaM-21 <merugu.akhila21@gmail.com>
2025-10-24 14:56:33 +0530

    latest version

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,3 @@
-// // API configuration
-// export const API_BASE_URL = 'http://localhost:8000';
-// // // // src/config/api.js
-export const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_API_URL
-  : 'http://localhost:8000'; // Your local backend URL
+
+export const API_BASE_URL = 'http://localhost:8000';
+

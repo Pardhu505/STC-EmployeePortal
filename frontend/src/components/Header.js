c1ec5d160a9bbdba064a63d96f8631453cfd05b6
AKhila <merugu.akhila21@gmail.com>
2025-12-06 12:46:13 +0530

    Remove social media links from Header component
    Removed social media links from the header component.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -113,21 +113,7 @@ const Header = ({ onSectionChange, newAnnouncements, onReadAnnouncement }) => {
             </div>
           </div>
 
-          {/* Social Media Links */}
-          <div className="hidden md:flex items-center space-x-4">
-            <a href="https://showtimeconsulting.in/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-[#225F8B]">
-              <Globe className="h-5 w-5" />
-              <span className="sr-only">Website</span>
-            </a>
-            <a href="https://www.instagram.com/showtime.consulting/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-[#225F8B]">
-              <Instagram className="h-5 w-5" />
-              <span className="sr-only">Instagram</span>
-            </a>
-            <a href="https://www.facebook.com/Showtimeconsulting/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-[#225F8B]">
-              <Facebook className="h-5 w-5" />
-              <span className="sr-only">Facebook</span>
-            </a>
-          </div>
+       
 
           {/* Right Side - Notifications and User Menu */}
           <div className="flex items-center space-x-4">

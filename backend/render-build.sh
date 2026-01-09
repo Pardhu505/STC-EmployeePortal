dbbd3542e15490a897cd502ca56525a80e247a47
AKhila <merugu.akhila21@gmail.com>
2026-01-09 17:58:49 +0530

    Update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -4,14 +4,17 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
+CHROME_BINARY="$STORAGE_DIR/chrome/opt/google/chrome/google-chrome"
+
+if [[ ! -f "$CHROME_BINARY" ]]; then
   echo "...Downloading Chrome"
+  rm -rf $STORAGE_DIR/chrome
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src
+  cd /opt/render/project/src
 else
   echo "...Using Chrome from cache"
 fi

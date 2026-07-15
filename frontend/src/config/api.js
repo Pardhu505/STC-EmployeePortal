6de4a8e06660f403fb9d55a2793f0d1a06fad19a
Pardhu505 <pardhasaradhi.thatimakula@gmail.com>
2026-07-15 10:01:25 +0530

    Fix api.js syntax error breaking the build

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,9 +1,1 @@
-
-<<<<<<< HEAD
-export const API_BASE_URL ='https://stcportal.duckdns.org'
-=======
-export const API_BASE_URL = 'https://stcportal.duckdns.org'
->>>>>>> 325683a9a7e102ad709285877c736ff15ec821ed
-
-
-
+export const API_BASE_URL = 'https://stcportal.duckdns.org';
